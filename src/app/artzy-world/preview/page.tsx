497634ed9a4c26efa4f66b9bf4f509bc2cf1e461
Jaisal Shah <jaisalshah@gmail.com>
2026-08-08 19:31:38 +0530

    Use canonical storefront preview URL

--- src/app/artzy-world/preview/page.tsx
+++ src/app/artzy-world/preview/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function ArtzyWorldPreviewPage() {
-  redirect('/artzy-world/preview-app/index.html?source=storefront-fullpage');
+  redirect('/artzy-world/preview-app/?source=storefront-fullpage');
 }
